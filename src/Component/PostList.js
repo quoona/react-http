b3846e9a5e9e02b23e7bad251f876ffc8f08b46f
Quoc Nam <nqnam229@gmail.com>
2021-09-09 07:30:09 +0700

    react-http get

---
+++ src/Component/PostList.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class PostList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts1")
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          posts: response.data,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        this.setState({ errorMsg: "Error Displaying Data" });
+      });
+  }
+
+  render() {
+    const { posts, errorMsg } = this.state;
+    return (
+      <div>
+        <h1>List Of Post</h1>
+        {posts.length
+          ? posts.map((post) => (
+              <div key={post.id}>
+                <h1>User Id: {post.userId}</h1>
+                <p>Title Post: {post.title}</p>
+              </div>
+            ))
+          : null}
+        {errorMsg ? <div>{errorMsg}</div> : null}
+      </div>
+    );
+  }
+}
+
+export default PostList;
